2a42b5e99870f1f522fb801375f349bba7882234
MurrayLisa <lisa.murray1605@gmail.com>
2019-04-15 21:32:56 +0100

    import data in python, for all 5 colums

--- analysis.py
+++ analysis.py
@@ -1,3 +1,5 @@
+#Pandas is an open source data analysis library
 import pandas as pd
 iris_data = pd.read_csv("data.csv")
-print (iris_data)
+iris_data_colums = ["sepal_length","Sepal_width", "Petal_length", "Petal width", "Species" ]
+print(iris_data)
