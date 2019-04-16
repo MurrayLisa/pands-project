80a27009aa5ceaeb245cf7a5a823540b29b2ddc4
MurrayLisa <lisa.murray1605@gmail.com>
2019-04-16 22:03:59 +0100

    Calculating minmum sepal length for iris-setosa

--- analysis.py
+++ analysis.py
@@ -1,7 +1,6 @@
-#Pandas is an open source data analysis library
 import pandas as pd
-iris_data = pd.read_csv("data.csv")
-iris_data_colums = ["sepal_length","Sepal_width", "Petal_length", "Petal width", "Class"]
-print(iris_data)
+iris_data = pd.read_csv('data.csv')
+iris_data2 = iris_data.set_index("class", drop = False)
+print (iris_data2.loc["Iris-setosa","sepallength"].min())
 
 
