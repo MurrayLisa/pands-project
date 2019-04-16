e8c85ecc266b4264189d678c496f6ad0b5b492cc
MurrayLisa <lisa.murray1605@gmail.com>
2019-04-16 21:38:07 +0100

    Created Jupyter folder

--- analysis.py
+++ analysis.py
@@ -1,5 +1,7 @@
 #Pandas is an open source data analysis library
 import pandas as pd
 iris_data = pd.read_csv("data.csv")
-iris_data_colums = ["sepal_length","Sepal_width", "Petal_length", "Petal width", "Species" ]
+iris_data_colums = ["sepal_length","Sepal_width", "Petal_length", "Petal width", "Class"]
 print(iris_data)
+
+
