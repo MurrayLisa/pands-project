1f7c51fdd79d0b8ce791714650a8d20cd85c5545
MurrayLisa <lisa.murray1605@gmail.com>
2019-04-23 15:25:06 +0100

    Added chart titles and added comment

--- analysis.py
+++ analysis.py
@@ -8,115 +8,135 @@ import numpy as np
 # Using seaborn library as it provides a high-level interface for drawing attractive statistical graphics.
 import seaborn as sns
 
+# Using matplotlib.pyplot library is useful for plotting graphs
 import matplotlib.pyplot as plt
 
-
+#Dataframe is a 2-dimensional labeled data structure with columns of potentially different types.  
 from pandas import DataFrame
-
 
 # the Iris data sets is being read through the csv file
 iris_data = pd.read_csv('data.csv')
 
-iris_data.head()
-
 # Output the results of the data set that is the number of colums and rows
-(iris_data.shape)
+print ('Number of Rows and Colums in data set:', iris_data.shape)
 
-
+# Analyses the Iris data set by class and size and prints out the number of data available per class/species
 iris_data['class'].unique()
-(iris_data.groupby('class').size())
+print (iris_data.groupby('class').size())
 'Iris-setosa', 'Iris-versicolor', 'Iris-virginica'
 
 iris_data2 = iris_data.set_index("class", drop = False)
 
-# Below data is a summary of all data, however it does not distunguish between the class/sprecies of iris
+# Prints a table with the summary of the class, count, mean, standard deviation, minimum, 25%, 50%, 75% and maximum values.
 summary = iris_data.describe()
 summary = summary.transpose()
-print (summary.head())
+print(summary.head())
 
+#Setting the formation and title layout of the graph
 iris_data.plot(kind='box', subplots=True, layout=(2,2), sharex=False, sharey=False)
-plt.gcf().canvas.set_window_title('Iris Attributes Comparsions for all Species')
-plt.show()
+plt.gcf().canvas.set_window_title('Summary of the Iris Flower Data Set for all Species') # Prunts title in pop up box
+plt.title('Summary of the Iris Flower Data Set',x=0, y=2.3)
+plt.show() # Prints Graph
 
-
-
-
+#Calculated the min sepal length for each of the species
 sepallength_min = [iris_data2.loc["Iris-setosa","sepallength"].min(), iris_data2.loc["Iris-versicolor","sepallength"].min(), iris_data2.loc["Iris-virginica","sepallength"].min()]
+#Calculated the mean sepal length for each of the species
 sepallength_mean = [iris_data2.loc["Iris-setosa","sepallength"].mean(), iris_data2.loc["Iris-versicolor","sepallength"].mean(),iris_data2.loc["Iris-virginica","sepallength"].mean()]
+#Calculated the max sepal length for each of the species
 sepallength_max = [iris_data2.loc["Iris-setosa","sepallength"].max(), iris_data2.loc["Iris-versicolor","sepallength"].max(), iris_data2.loc["Iris-virginica","sepallength"].max()]
 
+#Calculated the min sepal width for each of the species
 sepalwidth_min = [iris_data2.loc["Iris-setosa","sepalwidth"].min(), iris_data2.loc["Iris-versicolor","sepalwidth"].min(), iris_data2.loc["Iris-virginica","sepalwidth"].min()]
+#Calculated the mean sepal width for each of the species
 sepalwidth_mean = [iris_data2.loc["Iris-setosa","sepalwidth"].mean(), iris_data2.loc["Iris-versicolor","sepalwidth"].mean(), iris_data2.loc["Iris-virginica","sepalwidth"].mean()]
+#Calculated the max sepal width for each of the species
 sepalwidth_max = [iris_data2.loc["Iris-setosa","sepalwidth"].max(), iris_data2.loc["Iris-versicolor","sepalwidth"].max(), iris_data2.loc["Iris-virginica","sepalwidth"].max()]
 
+#Calculated the min petal length for each of the species
 petallength_min = [iris_data2.loc["Iris-setosa","petallength"].min(), iris_data2.loc["Iris-versicolor","petallength"].min(), iris_data2.loc["Iris-virginica","petallength"].min()]
+#Calculated the mean petal length for each of the species
 petallength_mean =[iris_data2.loc["Iris-setosa","petallength"].mean(), iris_data2.loc["Iris-versicolor","petallength"].mean(), iris_data2.loc["Iris-virginica","petallength"].mean()]
+#Calculated the max petal length for each of the species
 petallength_max = [iris_data2.loc["Iris-setosa","petallength"].max(), iris_data2.loc["Iris-versicolor","petallength"].max(), iris_data2.loc["Iris-virginica","petallength"].max()]
 
+#Calculated the min petal width for each of the species
 petalwidth_min = [iris_data2.loc["Iris-setosa","petalwidth"].min(), iris_data2.loc["Iris-versicolor","petalwidth"].min(), iris_data2.loc["Iris-virginica","petalwidth"].min()]
+#Calculated the mean petal width for each of the species
 petalwidth_mean = [iris_data2.loc["Iris-setosa","petalwidth"].mean(), iris_data2.loc["Iris-versicolor","petalwidth"].mean(), iris_data2.loc["Iris-virginica","petalwidth"].mean()]
+#Calculated the max petal width for each of the species
 petalwidth_max = [iris_data2.loc["Iris-setosa","petalwidth"].max(), iris_data2.loc["Iris-versicolor","petalwidth"].max(), iris_data2.loc["Iris-virginica","petalwidth"].max()]
 
+#Setting the parameters for plotting a bar chart, setting columns, labels, graph size and pop up chart name
 a=np.array([sepallength_min, sepallength_mean, sepallength_max, sepalwidth_min, sepalwidth_mean, sepalwidth_max])
 df=pd.DataFrame(a, columns=['Iris-setosa','Iris-versicolor ','Iris-virginica'], index=['sepallength_min','sepallength_mean','sepallength_max', 'sepalwidth_min', 'sepalwidth_mean', 'sepalwidth_max'])
 
+#Plotting the data for Sepal Comparsions
 df.plot(kind='bar', rot= 0, figsize= (10,5)) 
-plt.gcf().canvas.set_window_title('sepal comparsions')
+plt.gcf().canvas.set_window_title('Sepal Comparsions')
+plt.title('Sepal Comparsions',x=0.5, y=1)
 plt.show()
 
-
+#Setting the parameters for plotting a bar chart, setting columns, labels, graph size and pop up chart name
 b=np.array([petallength_min, petallength_mean, petallength_max, petalwidth_min, petalwidth_mean, petalwidth_max])
 df=pd.DataFrame(b, columns=['Iris-setosa','Iris-versicolor ','Iris-virginica'], index=['petallength_min','petallength_mean','petallength_max', 'petalwidth_min', 'petalwidth_mean', 'petalwidth_max'])
+
+#Plotting the data for Petal Comparsions
 df.plot(kind='bar', rot= 0, figsize= (10,5)) 
-plt.gcf().canvas.set_window_title('petal comparsions')
+plt.gcf().canvas.set_window_title('Petal Comparsions')
+plt.title('Petal Comparsions',x=0.5, y=1)
 plt.show()
 
-
-
-# plotting box graph for sepal length
+# plotting box graph for Sepal Length
 sns.boxplot(x="class", y="sepallength", data=iris_data)
-plt.title('Sepal Length Comparsions')
+plt.title('Sepal Length Comparsions',x=0.5, y=1)
 plt.gcf().canvas.set_window_title('Sepal Length')
 plt.show()
 
+# plotting box graph for Sepal Width
 sns.boxplot(x="class", y="sepalwidth", data=iris_data)
 plt.title('Sepal Width Comparsions')
 plt.gcf().canvas.set_window_title('Sepal Width')
 plt.show()
 
+# plotting box graph for Petal Length
 sns.boxplot(x="class", y="petallength", data=iris_data)
 plt.title('Petal Length Comparsions')
 plt.gcf().canvas.set_window_title('Petal Length')
 plt.show()
 
+# plotting box graph for Petal Width
 sns.boxplot(x="class", y="petalwidth", data=iris_data)
 plt.title('Petal Width Comparsions')
 plt.gcf().canvas.set_window_title('Petal Width')
 plt.show()
 
-
+# plotting a violin graph and setting graph parameters for Sepal Length Comparsions
 sns.violinplot(x="class", y="sepallength", data=iris_data)
 plt.title('Sepal Length Comparsions')
 plt.gcf().canvas.set_window_title('Sepal Length')
 plt.show()
 
+# plotting a violin graph and setting graph parameters for Sepal Width Comparsions
 sns.violinplot(x="class", y="sepalwidth", data=iris_data)
 plt.title('Sepal Width Comparsions')
 plt.gcf().canvas.set_window_title('Sepal Width')
 plt.show()
 
+# plotting a violin graph and setting graph parameters for Petal Length Comparsions
 sns.violinplot(x="class", y="petallength", data=iris_data)
 plt.title('Petal Length Comparsions')
 plt.gcf().canvas.set_window_title('Petal Length')
 plt.show()
 
+# plotting a violin graph and setting graph parameters for Petal Width Comparsions
 sns.violinplot(x="class", y="petalwidth", data=iris_data)
 plt.title('Petal Width Comparsions')
 plt.gcf().canvas.set_window_title('Petal Width')
 plt.show()
 
-
+#simplifying the data set to plot a swarmplot that shows the data without over lapping the data points 
 dataset = pd.melt(iris_data, "class", var_name="Attrubute")
 sns.swarmplot(x="Attrubute", y="value", hue="class", data=dataset)
+plt.title('Swarm Plot of all Iris Data')
 plt.gcf().canvas.set_window_title('Swarm Plot of all Iris Data')
 plt.show()
